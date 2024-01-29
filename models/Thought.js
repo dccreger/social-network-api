73f198cc13f6d2fe62b0655df63f361467133b2b
Chance Creger <chance.creger@gmail.com>
2024-01-29 09:54:59 -0700

    Created models for the Social Network API

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const {Schema, Types} = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),   
+        },
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [{
+            type: Schema.Types.ObjectId,
+            ref: 'reaction'
+        }],
+    },
+    {
+        // Enable virtuals to be included in JSON output
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    });
+
+thoughtSchema.virtual('formattedCreatedAt').get(function () {
+    return this.createdAt.toLocaleString();
+})
+
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+})
+
+const Thought = model('thought', thoughtSchema)
+module.exports = Thought;
